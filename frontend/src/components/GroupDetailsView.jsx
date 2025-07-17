0ab622c8010f2ea1ca86151d3e94ec174c48c51a
bhanukiran1535 <bhanukiran1535@gmail.com>
2025-07-17 13:45:45 +0530

    pull commit

--- frontend/src/components/GroupDetailsView.jsx
+++ frontend/src/components/GroupDetailsView.jsx
@@ -10,7 +10,7 @@ export const GroupDetailsView = ({ group, onClose }) => {
 
   const API_BASE = import.meta.env.VITE_API_BASE_URL;
 
-  useEffect(() => {
+  useEffect(() =>{
     if (group) {
       fetchGroupMonths();
     }
